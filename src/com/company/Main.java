5810dffad7d77161dad92ef19b8de9da2a097419
cyrrius <cyrrius@gmail.com>
2021-07-15 11:17:28 -0500

    final refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,6 +17,9 @@ public class Main {
 
         highScorePosition = calculateHighScorePosition(50);
         displayHighScorePosition("Ted", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Louise", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playersName, int highScorePosition) {
@@ -28,20 +31,27 @@ public class Main {
     public static int calculateHighScorePosition(int playerScore) {
         int highScorePosition;
 
-        if(playerScore > 1000) {
-            highScorePosition = 1;
-            return highScorePosition;
-        } else if(playerScore > 500 && playerScore < 1000) {
-            highScorePosition = 2;
-            return highScorePosition;
-        } else if(playerScore > 100 && playerScore < 500) {
-            highScorePosition = 3;
-            return highScorePosition;
-        } else {
-            highScorePosition = 4;
-            return highScorePosition;
+//        if(playerScore >= 1000) {
+//            return 1;
+//        } else if(playerScore >= 500) {
+//            return 2;
+//        } else if(playerScore >= 100) {
+//            return 3;
+//        }
+//
+//        return 4; // if all if statements fail, 4 is returned
+
+        int position = 4;
+
+        if(playerScore >= 1000) {
+            position = 1;
+        } else if(playerScore >= 500) {
+            position = 2;
+        } else if(playerScore >= 100) {
+            position = 3;
         }
 
+        return position;
 
     }
 }
